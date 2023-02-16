5ca798aacde0c9ba11c8affa57dc31f40ce47389
Artem Sergeichev <a.sergeichev03@mail.ru>
2023-02-16 11:07:23 +0300

    sdfg

--- REG_MARK_LIB/Class1.cs
+++ REG_MARK_LIB/Class1.cs
@@ -22,24 +22,59 @@ namespace REG_MARK_LIB
         string GetNextMarkAfter(string mark) // Метод для выдачи следующего номерного знака (либо следующего по номеру, либо следующего по серии)
         {
             int numberMark = Convert.ToInt32(mark.Substring(1, 4)); // Вынос цифр из номерного знака в отдельную переменную
+            string seriesMark = "";
             if (numberMark < 999)
             {
                 int newNumberMark = numberMark++;
-                string seriesMark = mark.Replace(Convert.ToString(numberMark), Convert.ToString(newNumberMark)); // Замена цифр в номерном знаке
+                seriesMark = mark.Replace(Convert.ToString(numberMark), Convert.ToString(newNumberMark)); // Замена цифр в номерном знаке
             }
             else
             {
                 int region = Convert.ToInt32(mark.Substring(6, 9)); // Вынос региона в отдельную переменную
-                string seriesMark = $"{mark[0] + mark[4] + mark[5]}"; // Вынос серии в отдельную переменную
+                seriesMark = $"{mark[0] + mark[4] + mark[5]}"; // Вынос серии в отдельную переменную
 
-                int positionLastLetterMark = 0, positionFirstLetterMark = 0;
+                //for (int i = 0; i < _markLetters.Length; i++)
+                //{
+                //    if (_markLetters[i] == seriesMark[2])
+                //    {
+                //        if (i == _markLetters.Length-1)
+                //        {
+                //            if (seriesMark[1] == seriesMark[2] && seriesMark[0] == seriesMark[2])
+                //            {
+                //                return "Нет больше номеров";
+                //            }
+                //            else if (seriesMark[1] == seriesMark[2])
+                //            {
+                //                seriesMark.Replace(seriesMark[2], _markLetters[0]);
+                //                seriesMark.Replace(seriesMark[1], _markLetters[0]);
+                //                seriesMark.Replace(seriesMark)
+                //            }
+                           
+                //        }
+                //    }
+                //}
+
+                int positionFirstLetter = 0, positionSecondLetter = 0, positionLastLetter = 0;
+                
                 for (int i = 0; i < _markLetters.Length; i++)
                 {
-                    if (_markLetters[i] == seriesMark[2])
+                    if (seriesMark[0] == _markLetters[i])
                     {
+                        positionFirstLetter = i;
+                    }
+                    
+                    if (seriesMark[1] == _markLetters[i])
+                    {
+                        positionSecondLetter = i;
+                    }
 
+                    if (seriesMark[2] == _markLetters[i])
+                    {
+                        positionLastLetter = i;
                     }
                 }
+
+                if (positionLastLetter == _markLetters)
             }
             return "";
         }
